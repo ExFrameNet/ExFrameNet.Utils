4378b735a90ba64eb9cad79a25ecc9e48c19bd2f
Yannick Schrade <yannick1218@outlook.com>
2022-04-13 13:59:40 +0200

    Added subscribe extension method and expression extensions

---
+++ src/xFrame.Extensions/System/ExpressionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace xFrame.Extensions.System
+{
+    public static class ExpressionExtensions
+    {
+        public static string GetPropertyName<T, TProperty>(this Expression<Func<T, TProperty>> expression)
+        {
+            var propInfo = expression.GetPropertyInfo();
+            return propInfo.Name;
+        }
+
+        public static PropertyInfo GetPropertyInfo<T, TProperty>(this Expression<Func<T, TProperty>> expression)
+        {
+            var member = expression.Body as MemberExpression;
+            if (member == null)
+                throw new ArgumentException($"Expression refers to a method");
+
+            var propInfo = member.Member as PropertyInfo;
+
+            if (propInfo == null)
+                throw new ArgumentException("Expression refers to a field");
+
+            return propInfo;
+        }
+    }
+}
